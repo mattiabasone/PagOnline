8843c3063c52762c8b6265f045fa6a38b0eeb213
Mattia Basone <mattia.basone@gmail.com>
2019-02-19 23:54:25 +0100

    Updated tests

--- tests/Unit/IgfsUtilsTest.php
+++ tests/Unit/IgfsUtilsTest.php
@@ -55,4 +55,12 @@ class IgfsUtilsTest extends TestCase
         $dateTime = new \DateTime('2019-02-19 00:00:00');
         $this->assertEquals($dateTime, IgfsUtils::parseXMLGregorianCalendar('19-Feb-2019 00:00:00'));
     }
+
+    /**
+     * @test
+     */
+    public function shouldNotReturnDateTimeClass()
+    {
+        $this->assertNull(IgfsUtils::parseXMLGregorianCalendar('2019-02-19 00:00:00'));
+    }
 }
