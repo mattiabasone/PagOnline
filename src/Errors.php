e5857ce3788ea0ba38621f24be4b3fad58d16253
Mattia Basone <mattia.basone@jointly.pro>
2019-10-25 09:41:19 +0200

    declares access to constants

--- src/Errors.php
+++ src/Errors.php
@@ -8,163 +8,163 @@ namespace PagOnline;
 final class Errors
 {
     // Ok statuses
-    const IGFS_000 = 'IGFS_000';
+    public const IGFS_000 = 'IGFS_000';
 
     // Pending statuses
-    const IGFS_814 = 'IGFS_814';
+    public const IGFS_814 = 'IGFS_814';
 
     // Fail statuses
-    const IGFS_001 = 'IGFS_001';
-    const IGFS_00155 = 'IGFS_00155';
-    const IGFS_00156 = 'IGFS_00156';
-    const IGFS_00157 = 'IGFS_00157';
-    const IGFS_00158 = 'IGFS_00158';
-    const IGFS_00159 = 'IGFS_00159';
+    public const IGFS_001 = 'IGFS_001';
+    public const IGFS_00155 = 'IGFS_00155';
+    public const IGFS_00156 = 'IGFS_00156';
+    public const IGFS_00157 = 'IGFS_00157';
+    public const IGFS_00158 = 'IGFS_00158';
+    public const IGFS_00159 = 'IGFS_00159';
 
-    const IGFS_002 = 'IGFS_002';
-    const IGFS_00260 = 'IGFS_00260';
-    const IGFS_00261 = 'IGFS_00261';
+    public const IGFS_002 = 'IGFS_002';
+    public const IGFS_00260 = 'IGFS_00260';
+    public const IGFS_00261 = 'IGFS_00261';
 
-    const IGFS_003 = 'IGFS_003';
-    const IGFS_004 = 'IGFS_004';
-    const IGFS_00452 = 'IGFS_00452';
-    const IGFS_00456 = 'IGFS_00456';
+    public const IGFS_003 = 'IGFS_003';
+    public const IGFS_004 = 'IGFS_004';
+    public const IGFS_00452 = 'IGFS_00452';
+    public const IGFS_00456 = 'IGFS_00456';
 
-    const IGFS_005 = 'IGFS_005';
+    public const IGFS_005 = 'IGFS_005';
 
-    const IGFS_006 = 'IGFS_006';
+    public const IGFS_006 = 'IGFS_006';
 
-    const IGFS_007 = 'IGFS_007';
+    public const IGFS_007 = 'IGFS_007';
 
-    const IGFS_00701 = 'IGFS_00701';
-    const IGFS_00704 = 'IGFS_00704';
-    const IGFS_00705 = 'IGFS_00705';
+    public const IGFS_00701 = 'IGFS_00701';
+    public const IGFS_00704 = 'IGFS_00704';
+    public const IGFS_00705 = 'IGFS_00705';
 
-    const IGFS_008 = 'IGFS_008';
+    public const IGFS_008 = 'IGFS_008';
 
-    const IGFS_009 = 'IGFS_009';
-    const IGFS_00950 = 'IGFS_00950';
-    const IGFS_00951 = 'IGFS_00951';
-    const IGFS_00952 = 'IGFS_00952';
+    public const IGFS_009 = 'IGFS_009';
+    public const IGFS_00950 = 'IGFS_00950';
+    public const IGFS_00951 = 'IGFS_00951';
+    public const IGFS_00952 = 'IGFS_00952';
 
-    const IGFS_010 = 'IGFS_010';
-    const IGFS_01000 = 'IGFS_01000';
+    public const IGFS_010 = 'IGFS_010';
+    public const IGFS_01000 = 'IGFS_01000';
 
-    const IGFS_011 = 'IGFS_011';
-    const IGFS_014 = 'IGFS_014';
-    const IGFS_015 = 'IGFS_015';
-    const IGFS_016 = 'IGFS_016';
-    const IGFS_018 = 'IGFS_018';
-    const IGFS_020 = 'IGFS_020';
-    const IGFS_021 = 'IGFS_021';
-    const IGFS_029 = 'IGFS_029';
-    const IGFS_030 = 'IGFS_030';
-    const IGFS_032 = 'IGFS_032';
-    const IGFS_033 = 'IGFS_033';
-    const IGFS_083 = 'IGFS_083';
-    const IGFS_085 = 'IGFS_085';
-    const IGFS_086 = 'IGFS_086';
-    const IGFS_087 = 'IGFS_087';
-    const IGFS_088 = 'IGFS_088';
-    const IGFS_091 = 'IGFS_091';
-    const IGFS_092 = 'IGFS_092';
-    const IGFS_093 = 'IGFS_093';
-    const IGFS_095 = 'IGFS_095';
-    const IGFS_096 = 'IGFS_096';
-    const IGFS_097 = 'IGFS_097';
-    const IGFS_098 = 'IGFS_098';
-    const IGFS_10000 = 'IGFS_10000';
-    const IGFS_101 = 'IGFS_101';
-    const IGFS_102 = 'IGFS_102';
-    const IGFS_104 = 'IGFS_104';
-    const IGFS_107 = 'IGFS_107';
-    const IGFS_108 = 'IGFS_108';
-    const IGFS_112 = 'IGFS_112';
-    const IGFS_115 = 'IGFS_115';
-    const IGFS_117 = 'IGFS_117';
-    const IGFS_118 = 'IGFS_118';
-    const IGFS_119 = 'IGFS_119';
-    const IGFS_121 = 'IGFS_121';
-    const IGFS_122 = 'IGFS_122';
-    const IGFS_123 = 'IGFS_123';
-    const IGFS_125 = 'IGFS_125';
-    const IGFS_129 = 'IGFS_129';
-    const IGFS_160 = 'IGFS_160';
-    const IGFS_164 = 'IGFS_164';
-    const IGFS_180 = 'IGFS_180';
-    const IGFS_181 = 'IGFS_181';
-    const IGFS_1921 = 'IGFS_1921';
-    const IGFS_1922 = 'IGFS_1922';
-    const IGFS_1923 = 'IGFS_1923';
-    const IGFS_20000 = 'IGFS_20000';
-    const IGFS_20001 = 'IGFS_20001';
-    const IGFS_20007 = 'IGFS_20007';
-    const IGFS_20010 = 'IGFS_20010';
-    const IGFS_20011 = 'IGFS_20011';
-    const IGFS_20012 = 'IGFS_20012';
-    const IGFS_20013 = 'IGFS_20013';
-    const IGFS_20014 = 'IGFS_20014';
-    const IGFS_20018 = 'IGFS_20018';
-    const IGFS_20019 = 'IGFS_20019';
-    const IGFS_20020 = 'IGFS_20020';
-    const IGFS_20021 = 'IGFS_20021';
-    const IGFS_20022 = 'IGFS_20022';
-    const IGFS_20023 = 'IGFS_20023';
-    const IGFS_20024 = 'IGFS_20024';
-    const IGFS_20025 = 'IGFS_20025';
-    const IGFS_20026 = 'IGFS_20026';
-    const IGFS_20027 = 'IGFS_20027';
-    const IGFS_20028 = 'IGFS_20028';
-    const IGFS_20029 = 'IGFS_20029';
-    const IGFS_20030 = 'IGFS_20030';
-    const IGFS_20031 = 'IGFS_20031';
-    const IGFS_20032 = 'IGFS_20032';
-    const IGFS_20033 = 'IGFS_20033';
-    const IGFS_20034 = 'IGFS_20034';
-    const IGFS_20035 = 'IGFS_20035';
-    const IGFS_20036 = 'IGFS_20036';
-    const IGFS_20037 = 'IGFS_20037';
-    const IGFS_20038 = 'IGFS_20038';
-    const IGFS_20044 = 'IGFS_20044';
-    const IGFS_20090 = 'IGFS_20090';
-    const IGFS_20100 = 'IGFS_20100';
+    public const IGFS_011 = 'IGFS_011';
+    public const IGFS_014 = 'IGFS_014';
+    public const IGFS_015 = 'IGFS_015';
+    public const IGFS_016 = 'IGFS_016';
+    public const IGFS_018 = 'IGFS_018';
+    public const IGFS_020 = 'IGFS_020';
+    public const IGFS_021 = 'IGFS_021';
+    public const IGFS_029 = 'IGFS_029';
+    public const IGFS_030 = 'IGFS_030';
+    public const IGFS_032 = 'IGFS_032';
+    public const IGFS_033 = 'IGFS_033';
+    public const IGFS_083 = 'IGFS_083';
+    public const IGFS_085 = 'IGFS_085';
+    public const IGFS_086 = 'IGFS_086';
+    public const IGFS_087 = 'IGFS_087';
+    public const IGFS_088 = 'IGFS_088';
+    public const IGFS_091 = 'IGFS_091';
+    public const IGFS_092 = 'IGFS_092';
+    public const IGFS_093 = 'IGFS_093';
+    public const IGFS_095 = 'IGFS_095';
+    public const IGFS_096 = 'IGFS_096';
+    public const IGFS_097 = 'IGFS_097';
+    public const IGFS_098 = 'IGFS_098';
+    public const IGFS_10000 = 'IGFS_10000';
+    public const IGFS_101 = 'IGFS_101';
+    public const IGFS_102 = 'IGFS_102';
+    public const IGFS_104 = 'IGFS_104';
+    public const IGFS_107 = 'IGFS_107';
+    public const IGFS_108 = 'IGFS_108';
+    public const IGFS_112 = 'IGFS_112';
+    public const IGFS_115 = 'IGFS_115';
+    public const IGFS_117 = 'IGFS_117';
+    public const IGFS_118 = 'IGFS_118';
+    public const IGFS_119 = 'IGFS_119';
+    public const IGFS_121 = 'IGFS_121';
+    public const IGFS_122 = 'IGFS_122';
+    public const IGFS_123 = 'IGFS_123';
+    public const IGFS_125 = 'IGFS_125';
+    public const IGFS_129 = 'IGFS_129';
+    public const IGFS_160 = 'IGFS_160';
+    public const IGFS_164 = 'IGFS_164';
+    public const IGFS_180 = 'IGFS_180';
+    public const IGFS_181 = 'IGFS_181';
+    public const IGFS_1921 = 'IGFS_1921';
+    public const IGFS_1922 = 'IGFS_1922';
+    public const IGFS_1923 = 'IGFS_1923';
+    public const IGFS_20000 = 'IGFS_20000';
+    public const IGFS_20001 = 'IGFS_20001';
+    public const IGFS_20007 = 'IGFS_20007';
+    public const IGFS_20010 = 'IGFS_20010';
+    public const IGFS_20011 = 'IGFS_20011';
+    public const IGFS_20012 = 'IGFS_20012';
+    public const IGFS_20013 = 'IGFS_20013';
+    public const IGFS_20014 = 'IGFS_20014';
+    public const IGFS_20018 = 'IGFS_20018';
+    public const IGFS_20019 = 'IGFS_20019';
+    public const IGFS_20020 = 'IGFS_20020';
+    public const IGFS_20021 = 'IGFS_20021';
+    public const IGFS_20022 = 'IGFS_20022';
+    public const IGFS_20023 = 'IGFS_20023';
+    public const IGFS_20024 = 'IGFS_20024';
+    public const IGFS_20025 = 'IGFS_20025';
+    public const IGFS_20026 = 'IGFS_20026';
+    public const IGFS_20027 = 'IGFS_20027';
+    public const IGFS_20028 = 'IGFS_20028';
+    public const IGFS_20029 = 'IGFS_20029';
+    public const IGFS_20030 = 'IGFS_20030';
+    public const IGFS_20031 = 'IGFS_20031';
+    public const IGFS_20032 = 'IGFS_20032';
+    public const IGFS_20033 = 'IGFS_20033';
+    public const IGFS_20034 = 'IGFS_20034';
+    public const IGFS_20035 = 'IGFS_20035';
+    public const IGFS_20036 = 'IGFS_20036';
+    public const IGFS_20037 = 'IGFS_20037';
+    public const IGFS_20038 = 'IGFS_20038';
+    public const IGFS_20044 = 'IGFS_20044';
+    public const IGFS_20090 = 'IGFS_20090';
+    public const IGFS_20100 = 'IGFS_20100';
 
-    const IGFS_400 = 'IGFS_400';
-    const IGFS_800 = 'IGFS_800';
-    const IGFS_801 = 'IGFS_801';
-    const IGFS_802 = 'IGFS_802';
-    const IGFS_803 = 'IGFS_803';
-    const IGFS_804 = 'IGFS_804';
-    const IGFS_805 = 'IGFS_805';
-    const IGFS_807 = 'IGFS_807';
-    const IGFS_808 = 'IGFS_808';
-    const IGFS_809 = 'IGFS_809';
-    const IGFS_810 = 'IGFS_810';
-    const IGFS_811 = 'IGFS_811';
-    const IGFS_812 = 'IGFS_812';
-    const IGFS_813 = 'IGFS_813';
-    const IGFS_815 = 'IGFS_815';
+    public const IGFS_400 = 'IGFS_400';
+    public const IGFS_800 = 'IGFS_800';
+    public const IGFS_801 = 'IGFS_801';
+    public const IGFS_802 = 'IGFS_802';
+    public const IGFS_803 = 'IGFS_803';
+    public const IGFS_804 = 'IGFS_804';
+    public const IGFS_805 = 'IGFS_805';
+    public const IGFS_807 = 'IGFS_807';
+    public const IGFS_808 = 'IGFS_808';
+    public const IGFS_809 = 'IGFS_809';
+    public const IGFS_810 = 'IGFS_810';
+    public const IGFS_811 = 'IGFS_811';
+    public const IGFS_812 = 'IGFS_812';
+    public const IGFS_813 = 'IGFS_813';
+    public const IGFS_815 = 'IGFS_815';
 
-    const IGFS_90000 = 'IGFS_90000';
-    const IGFS_90005 = 'IGFS_90005';
+    public const IGFS_90000 = 'IGFS_90000';
+    public const IGFS_90005 = 'IGFS_90005';
 
-    const IGFS_902 = 'IGFS_902';
-    const IGFS_903 = 'IGFS_903';
-    const IGFS_907 = 'IGFS_907';
-    const IGFS_908 = 'IGFS_908';
-    const IGFS_909 = 'IGFS_909';
-    const IGFS_910 = 'IGFS_910';
-    const IGFS_911 = 'IGFS_911';
-    const IGFS_912 = 'IGFS_912';
-    const IGFS_913 = 'IGFS_913';
-    const IGFS_990 = 'IGFS_990';
+    public const IGFS_902 = 'IGFS_902';
+    public const IGFS_903 = 'IGFS_903';
+    public const IGFS_907 = 'IGFS_907';
+    public const IGFS_908 = 'IGFS_908';
+    public const IGFS_909 = 'IGFS_909';
+    public const IGFS_910 = 'IGFS_910';
+    public const IGFS_911 = 'IGFS_911';
+    public const IGFS_912 = 'IGFS_912';
+    public const IGFS_913 = 'IGFS_913';
+    public const IGFS_990 = 'IGFS_990';
 
     /**
      * Array with all messages.
      *
      * @var array
      */
-    const MESSAGES = [
+    public const MESSAGES = [
         self::IGFS_000 => 'TRANSACTION OK',
 
         // Pending statuses
@@ -258,8 +258,8 @@ final class Errors
         self::IGFS_20013 => 'INVALID LANGUAGE CODE',
         self::IGFS_20014 => 'INVALID ADDITIONAL FIELD',
         self::IGFS_20018 => 'INVALID CVV2',
-        self::IGFS_20019 => 'SHOPID NON VALIDO', // TODO: Duplicates IGFS_20012
-        self::IGFS_20020 => 'INVALID ADDITIONAL FIELD', // TODO: Duplicates IGFS_20014
+        self::IGFS_20019 => 'SHOPID NON VALIDO', // Duplicates IGFS_20012
+        self::IGFS_20020 => 'INVALID ADDITIONAL FIELD', // Duplicates IGFS_20014
         self::IGFS_20021 => 'INVALID API VERSION FIELD',
         self::IGFS_20022 => 'INVALID SIGNATURE FIELD',
         self::IGFS_20023 => 'INVALID PAYMENT ID FIELD',
@@ -322,10 +322,6 @@ final class Errors
      */
     public static function getMessage($errorCode): ?string
     {
-        if (\array_key_exists($errorCode, self::MESSAGES)) {
-            return self::MESSAGES[$errorCode];
-        } else {
-            return null;
-        }
+        return self::MESSAGES[$errorCode] ?? null;
     }
 }
